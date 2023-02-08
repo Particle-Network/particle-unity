9341a9841d5b46c8923c724016dcc2c2b848c264
Zhichao Link <sunzhichao@minijoy.work>
2023-02-08 10:48:32 +0800

    add more examples

--- Assets/ParticleNetwork/Scripts/Demo/Example.cs
+++ Assets/ParticleNetwork/Scripts/Demo/Example.cs
@@ -21,14 +21,34 @@ namespace Network.Particle.Scripts.Test
             
             // Set support chain info array. you can a chain info array.
             ParticleWalletGUI.SupportChain(new []{chainInfo});
-            // Disable buy 
+            // Disable buy.
             ParticleWalletGUI.EnablePay(false);
-            // Disable testnet if release
+            // Disable testnet if release.
             ParticleWalletGUI.ShowTestNetwork(false);
-            // Disable wallet manage page if you only support one wallet
+            // Disable wallet manage page if you only support one wallet.
             ParticleWalletGUI.ShowManageWallet(false);
             // Use this method to control dark mode or light mode. you can call this method with your button.
             ParticleNetwork.SetInterfaceStyle(UserInterfaceStyle.DARK);
+            
+            // Manage Tokens and NFTs, set show only native and your tokens, NFTs. don't show other tokens and NFTs.
+            ParticleWalletGUI.SetDisplayTokenAddresses(new []{"Your token address"});
+            ParticleWalletGUI.SetDisplayNFTContractAddresses(new []{"Your nft address"});
+            
+            // Control if show add button in wallet page.
+            ParticleWalletGUI.SetSupportAddToken(false);
+            
+            // Control UI pages native currency symbol
+            ParticleWalletGUI.SetFiatCoin("HKD");
+            
+            // Set language
+            ParticleWalletGUI.SetLanguage(Language.KO);
+            
+            // Control if show language setting button in setting page.
+            ParticleWalletGUI.ShowLanguageSetting(true);
+            
+            // Control if show appearance setting button in setting page.
+            ParticleWalletGUI.ShowAppearanceSetting(true);
+            
         }
 
         public async void Login()
