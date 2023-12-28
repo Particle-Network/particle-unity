7a61f02011ba53eb68a39316280f9305be772aec
Zhichao Link <sunzhichao@minijoy.work>
2023-12-28 15:52:31 +0800

    update

--- Assets/ParticleNetwork/Mobile/Modules/Core/Model/ConnectConfig.cs
+++ Assets/ParticleNetwork/Mobile/Modules/Core/Model/ConnectConfig.cs
@@ -21,7 +21,9 @@ namespace Network.Particle.Scripts.Model
         /// <param name="supportAuthTypes">Controls whether third-party login buttons are displayed.</param>
         /// <param name="socialLoginPrompt">Optional, controls whether show light UI in web, default is false.</param>
         /// <param name="authorization">Optional, LoginAuthorization, , login and sign message, its message requires hex in evm, base58 in solana </param>
-        public ConnectConfig(LoginType loginType, [CanBeNull] string account,[CanBeNull] string code, SupportAuthType supportAuthTypes, [CanBeNull] LoginAuthorization authorization = null, SocialLoginPrompt? socialLoginPrompt = null)
+        public ConnectConfig(LoginType loginType, [CanBeNull] string account, [CanBeNull] string code,
+            SupportAuthType supportAuthTypes, [CanBeNull] LoginAuthorization authorization = null,
+            SocialLoginPrompt? socialLoginPrompt = null)
         {
             this.loginType = loginType;
             this.account = account;
@@ -29,29 +31,6 @@ namespace Network.Particle.Scripts.Model
             this.supportAuthTypes = supportAuthTypes;
             this.socialLoginPrompt = socialLoginPrompt;
             this.authorization = authorization;
-        }
-    }
-    
-    public class ParticleAuthCoreConnectConfig
-    {
-        public LoginType loginType;
-        [CanBeNull] public string account;
-        [CanBeNull] public string code;
-        public SocialLoginPrompt? socialLoginPrompt;
-
-        /// <summary>
-        /// Particle connect configuration
-        /// </summary>
-        /// <param name="loginType">Login type, support email, phone, json web token, google, apple and more</param>
-        /// <param name="account">Account, such as phone number, email, json web token.</param>
-        /// <param name="code">Email or phone code</param>
-        /// <param name="socialLoginPrompt">Controls whether show light UI in web, default is false.</param>
-        public ParticleAuthCoreConnectConfig(LoginType loginType, [CanBeNull] string account, [CanBeNull] string code, SocialLoginPrompt? socialLoginPrompt = null)
-        {
-            this.loginType = loginType;
-            this.account = account;
-            this.code = code;
-            this.socialLoginPrompt = socialLoginPrompt;
         }
     }
 }
