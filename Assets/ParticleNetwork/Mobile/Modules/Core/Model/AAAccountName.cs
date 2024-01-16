5656d46f5d0702e734a7f098ef6b4751d4128bd3
Zhichao Link <sunzhichao@minijoy.work>
2024-01-16 16:15:07 +0800

    Add Light Account Update chaininfo

--- Assets/ParticleNetwork/Mobile/Modules/Core/Model/AAAccountName.cs
+++ Assets/ParticleNetwork/Mobile/Modules/Core/Model/AAAccountName.cs
@@ -12,24 +12,49 @@ namespace Network.Particle.Scripts.Model
             this.version = version;
         }
 
+        /// <summary>
+        /// Biconomy Account 1.0.0
+        /// </summary>
+        /// <returns></returns>
         public static AAAccountName BICONOMY_V1()
         {
             return new AAAccountName("BICONOMY", "1.0.0");
         }
 
+        /// <summary>
+        /// Biconomy Account 2.0.0
+        /// </summary>
+        /// <returns></returns>
         public static AAAccountName BICONOMY_V2()
         {
             return new AAAccountName("BICONOMY", "2.0.0");
         }
 
+        /// <summary>
+        /// Simple Account 1.0.0
+        /// </summary>
+        /// <returns></returns>
         public static AAAccountName SIMPLE()
         {
             return new AAAccountName("SIMPLE", "1.0.0");
         }
 
+        /// <summary>
+        /// Cyber Account 1.0.0
+        /// </summary>
+        /// <returns></returns>
         public static AAAccountName CYBERCONNECT()
         {
             return new AAAccountName("CYBERCONNECT", "1.0.0");
+        }
+
+        /// <summary>
+        /// Alchemy Light Account 1.0.2
+        /// </summary>
+        /// <returns></returns>
+        public static AAAccountName LIGHT()
+        {
+            return new AAAccountName("LIGHT", "1.0.2");
         }
     }
 }
